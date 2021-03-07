24b726c7cb44af549a37fd50f6598516167ff3c5
Nirbhay <mauryanirbhay1002@gmail.com>
2021-03-07 21:42:30 +0530

    anomaly detection and recommender system

---
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -0,0 +1,43 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+% You should return these values correctly
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the mean of the data and the variances
+%               In particular, mu(i) should contain the mean of
+%               the data for the i-th feature and sigma2(i)
+%               should contain variance of the i-th feature.
+%
+mu=(1/m)*sum(X);
+for i=1:m
+ D(i,:)=mu;   %to make a matrix of size (mxn), each row equal to mu
+end
+mu=mu(:);
+sigma2=(1/m)*sum((X-D).^2);
+sigma2=sigma2(:);
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
